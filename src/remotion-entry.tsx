ab6c7d79f5cd4d1a701ed2347fafd90c100cce97
kuro-usagi-labs <kusagi.gf@gmail.com>
2026-07-13 01:39:47 +0700

    Calculate Remotion metadata from the active scene

--- src/remotion-entry.tsx
+++ src/remotion-entry.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
-import {Composition, registerRoot} from 'remotion';
-import {MotionComposition} from './MotionComposition';
-import {starterProject, type Project} from './types';
+import React from "react";
+import { Composition, registerRoot } from "remotion";
+import { MotionComposition } from "./MotionComposition";
+import { createStarterProject, getActiveScene } from "./core/project";
+import type { KurogiProject } from "./types";
 
-const Root: React.FC = () => <Composition id="KurogiMotion" component={MotionComposition} durationInFrames={150} fps={30} width={1080} height={1080} defaultProps={{project: starterProject as Project}} calculateMetadata={({props}) => ({durationInFrames: Math.round(props.project.duration * props.project.fps), fps: props.project.fps, width: props.project.width, height: props.project.height})}/>;
+const defaultProject = createStarterProject();
+
+const Root: React.FC = () => (
+  <Composition
+    id="KurogiMotion"
+    component={MotionComposition}
+    durationInFrames={150}
+    fps={30}
+    width={1080}
+    height={1080}
+    defaultProps={{ project: defaultProject as KurogiProject }}
+    calculateMetadata={({ props }) => {
+      const scene = getActiveScene(props.project);
+      return {
+        durationInFrames: Math.max(1, Math.round(scene.duration * scene.fps)),
+        fps: scene.fps,
+        width: scene.width,
+        height: scene.height,
+      };
+    }}
+  />
+);
+
 registerRoot(Root);

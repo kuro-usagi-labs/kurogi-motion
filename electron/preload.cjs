172fd7fa5efa7727f9e11c5434edf3cad5de88f5
kuro-usagi-labs <kusagi.gf@gmail.com>
2026-07-13 03:18:57 +0700

    Expose native kuromotion file dialogs

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,6 +3,8 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("kurogi", {
   platform: process.platform,
   exportVideo: (project, options) => ipcRenderer.invoke("export-video", project, options),
+  saveKuroMotionFile: (envelope, defaultName) => ipcRenderer.invoke("save-kuromotion-file", envelope, defaultName),
+  openKuroMotionFile: () => ipcRenderer.invoke("open-kuromotion-file"),
   onExportProgress: (listener) => {
     const handler = (_event, progress) => listener(progress);
     ipcRenderer.on("export-progress", handler);

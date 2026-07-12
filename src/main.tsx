483b6253de997600d4ec818ad4974191a7b6a1fd
kuro-usagi-labs <kusagi.gf@gmail.com>
2026-07-13 03:21:23 +0700

    Load product polish v3

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./overrides.css";
 import "./product.css";
 import "./polish.css";
+import "./polishV3.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Kurogi Motion could not find the application root.");

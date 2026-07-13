151b5e70ae8f38520a6219e51d6b289d1b1c2f93
kuro-usagi-labs <kusagi.gf@gmail.com>
2026-07-14 04:42:25 +0700

    feat: mount project transfer tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
+import { ProjectTransferDock } from "./app/ProjectTransferDock";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Kurogi Motion root element was not found");
@@ -8,5 +9,6 @@ if (!root) throw new Error("Kurogi Motion root element was not found");
 createRoot(root).render(
   <React.StrictMode>
     <App />
+    <ProjectTransferDock />
   </React.StrictMode>,
 );

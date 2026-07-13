6d56d3685d3bc00d40729590d4e66401eb8f421a
kuro-usagi-labs <kusagi.gf@gmail.com>
2026-07-13 10:11:24 +0700

    Load expanded motion library styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./product.css";
 import "./polish.css";
 import "./polishV3.css";
 import "./finalUx.css";
+import "./motionLibrary.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Kurogi Motion could not find the application root.");
